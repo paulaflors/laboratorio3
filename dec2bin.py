f1c73a482eb072629bc44f06e9d97c49d138a7aa
Adolfo Martinez <adolfo.mar.gar.07@gmail.com>
2024-10-03 17:58:38 +0200

    Creating conflict in merge

--- dec2bin.py
+++ dec2bin.py
@@ -1,7 +1,7 @@
 # ------------------------------------------------------
 # Convierte un número decimal positivo a binario usando un 
 #   determinado número de bits.
-# El binario resultante es un string e.g. "101"
+# hola estoy causando un conflicto con mi compañero
 # Se usa la función bin() que transforma e.g. 3 en "0b11".
 # En esta función se quita el "0b" para dejar el "11"
 # ------------------------------------------------------
